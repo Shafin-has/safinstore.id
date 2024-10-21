4311ad5f8dd61f4ebffc9bf3df02c4fe532a1947
Iqbal Novrian <bcoding56@gmail.com>
2024-10-21 08:10:57 +0800

    Modiffied

--- script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.getElementById("menu");
-  const navbarLinks = document.getElementById("navbar-links");
-
-  menuButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-  });
-});
